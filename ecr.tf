e64dd08a8d519f7e5ea63ea3fffc5e632d07bc6f
g31s <gurjant1337@gmail.com>
2022-09-10 06:34:14 +0000

    [+] minor vars fixed

--- ecr.tf
+++ ecr.tf
@@ -9,7 +9,7 @@ This file will create following:
 // create ecr registry
 resource "aws_ecr_repository" "ecr_repo" {
   // run only if app image is not provided and don't if virtual gateway is provided
-  count = (var.app_image != "none" || var.virtual_gateway != "none") ? 0 : 1
+  count = (var.app_image != "none" || var.virtual_gateway_arn != "none") ? 0 : 1
   // name can be in lower case only
   name                 = lower("${var.prefix}-${var.env}-${var.app_name}")
   image_tag_mutability = "IMMUTABLE"
